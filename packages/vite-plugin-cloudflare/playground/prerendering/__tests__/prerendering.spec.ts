5eba7300ae248ed232fe46e19f05fbae2bffd251
James Opstad <13586373+jamesopstad@users.noreply.github.com>
2025-12-18 14:00:00 +0000

    Skip prerendering test on Vite 6 because buildApp hook is not supported

--- packages/vite-plugin-cloudflare/playground/prerendering/__tests__/prerendering.spec.ts
+++ packages/vite-plugin-cloudflare/playground/prerendering/__tests__/prerendering.spec.ts
@@ -3,6 +3,7 @@ import {
 	getTextResponse,
 	isBuild,
 	page,
+	satisfiesViteVersion,
 	viteTestUrl,
 } from "../../__test-utils__";
 
@@ -10,7 +11,7 @@ test("returns the server rendered route at /", async () => {
 	expect(await getTextResponse()).toEqual("Hello world");
 });
 
-test.runIf(isBuild)(
+test.runIf(isBuild && satisfiesViteVersion("7.0.0"))(
 	"returns the prerendered route at /prerendered after the build",
 	async () => {
 		await page.goto(`${viteTestUrl}/prerendered`);
